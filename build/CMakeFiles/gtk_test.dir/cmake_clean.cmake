0fec72db0769b50b245c11f51ed4a466c0c3e279
NicolaPiemontese <nicola.piemontese@protonmail.com>
2021-11-10 11:19:02 +0100

    rules

---
+++ build/CMakeFiles/gtk_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtk_test.dir/main.c.o"
+  "CMakeFiles/gtk_test.dir/main.c.o.d"
+  "gtk_test"
+  "gtk_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gtk_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

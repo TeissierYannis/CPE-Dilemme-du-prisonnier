79ff13bf80d1a72364fe3ba51acd52a1e9452bad
AlexisRevol <revol.alexis.lgm@gmail.com>
2021-11-11 17:25:54 +0100

    Test socket client

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+    printf("hello qui");
+    int i = 0;
+    for (int j = i;  j<5; j++){
+        printf("boucle\n");
+    }
+
+    return 0;
+}

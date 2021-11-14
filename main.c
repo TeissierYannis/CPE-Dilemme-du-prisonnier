ebd1407d27c6d18f9666e766af7faa653f2520b3
NicolaPiemontese <nicola.piemontese@protonmail.com>
2021-11-14 18:28:23 +0100

    Reading configuration rules

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "headers/read_rules.h"
+#include "headers/string_utils.h"
+
 int main(int argc, char **argv)
 {
-    printf("Hello World!");
+    printf("Hello World!\n");
+    read_rules();
     return EXIT_SUCCESS;
 }

e637c1d1cb28e947eebdf41b53da51d605a38fbb
SAGLAMER <trk.0506@hotmail.com>
2021-11-24 11:55:31 +0100

    bouton quitter

--- main.c
+++ main.c
@@ -13,24 +13,24 @@ GtkWidget *fixed1;
 GtkWidget *trahison;
 GtkWidget *collaboration;
 GtkWidget *quitter;
+GtkWidget *rounde;
 GtkWidget *titre;
 GtkWidget *score1;
 GtkWidget *score2;
 GtkBuilder *builder;
-
 
 int main(int argc, char **argv)
 {
     gtk_init(&argc, &argv); // init Gtk
     builder = gtk_builder_new_from_file("../glade/glade.glade");
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_builder_connect_signals(builder, NULL);
 
     fixed1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed1"));
     trahison = GTK_WIDGET(gtk_builder_get_object(builder, "trahison"));
     collaboration = GTK_WIDGET(gtk_builder_get_object(builder, "collaboration"));
     quitter = GTK_WIDGET(gtk_builder_get_object(builder, "quitter"));
+    rounde = GTK_WIDGET(gtk_builder_get_object(builder, "rounde"));
     titre = GTK_WIDGET(gtk_builder_get_object(builder, "titre"));
     score1 = GTK_WIDGET(gtk_builder_get_object(builder, "score1"));
     score2 = GTK_WIDGET(gtk_builder_get_object(builder, "score2"));
@@ -38,4 +38,25 @@ int main(int argc, char **argv)
     gtk_widget_show(window);
     gtk_main();
     return (EXIT_SUCCESS);
+}
+
+void on_trahison_clicked(GtkButton *b)
+{
+    int point = 1;
+    int nbround = 10;
+    char str_count[30] = {0};
+    for(int i = 0 ; i<nbround; i++){
+        point =+ i;
+    }
+
+    sprintf(str_count, "%d", point);
+
+    gtk_label_set_text(GTK_LABEL(rounde), str_count);
+}
+
+void on_collaboration_clicked() {}
+
+void on_quitter_clicked()
+{
+    gtk_main_quit();
 }
